14901370505ac00db8c8ee96b96cb21e7b8d70ec
Jan Huang <35048993+janhuang6@users.noreply.github.com>
2019-03-06 13:11:55 -0800

    Update FormatConverterJson2IOB.py

--- FormatConverterJson2IOB.py
+++ FormatConverterJson2IOB.py
@@ -1,47 +1,83 @@
 import json
 
 if __name__=="__main__":
-    with open("C:\\projects\\datasets\\nlu-benchmark\\2017-06-custom-intent-engines\\SearchScreeningEvent\\validate_SearchScreeningEvent.json", "r", encoding="utf-8") as data_file:
-        data = json.load(data_file)
+    intent = "GetWeather"
+    base_path = "C:\\projects\\datasets\\nlu-benchmark\\2017-06-custom-intent-engines\\"
 
-    intent = "SearchScreeningEvent"
+    input_validate = base_path + intent + "\\validate_" + intent + ".json"
+    input_train = base_path + intent + "\\train_" + intent + ".json"
+    input_train_full = base_path + intent + "\\train_" + intent + "_full.json"
 
-    output_file = open("C:\\projects\\datasets\\nlu-benchmark\\2017-06-custom-intent-engines\\SearchScreeningEvent\\validate_SearchScreeningEvent.iob", "w", encoding="utf-8")
+    output_validate = base_path + intent + "\\validate_" + intent + ".iob"
+    output_train = base_path + intent + "\\train_" + intent + ".iob"
+    output_train_full = base_path + intent + "\\train_" + intent + "_full.iob"
 
-    for utterance in data[intent]:
-        origin = "BOS "
-        label = ""
-        for item in utterance["data"]:
-            text = item["text"]
+    io_validate = (input_validate, output_validate)
+    io_train = (input_train, output_train)
+    io_train_full = (input_train_full, output_train_full)
 
-            if origin[len(origin)-1] != " " and text[0] != " ":
-                origin += " "
-            origin += text
-            if "entity" in item:
+    ios = (io_validate, io_train, io_train_full)
+
+    output_stats = open(base_path + intent + "\\" + intent + "_stats.txt", "w", encoding="utf-8")
+
+    total_wc = 0
+    entity_wc = {}
+
+    for (input_file, output_file) in ios:
+        with open(input_file, "r", encoding="utf-8") as data_file:
+            data = json.load(data_file)
+
+        output_iob = open(output_file, "w", encoding="utf-8")
+
+        for utterance in data[intent]:
+            origin = "BOS "
+            label = "O"
+            for item in utterance["data"]:
+                text = item["text"]
+
+                if text.isspace() or len(text) == 0:
+                    continue
+                text = text.replace('\n', ' ')
+                text = text.replace('\t', ' ')
+
+                if origin[len(origin)-1] != " " and text[0] != " ":
+                    origin += " "
+                origin += text
                 split = text.split()
-                for x in range(len(split)):
-                    if x == 0:
-                        if label != "":
-                            label += " "
-                        label += "B_" + item["entity"]
-                    else:
-                        if label != "":
-                            label += " "
-                        label += "I_" + item["entity"]
-            else:
-                split = text.split()
-                for x in range(len(split)):
-                    if label != "":
-                        label += " "
-                    label += "O"
+                if "entity" in item:
+                    total_wc += 1
+                    if item["entity"] not in entity_wc:
+                        entity_wc[item["entity"]] = 0
+                    entity_wc[item["entity"]] += 1
+                    for x in range(len(split)):
+                        if x == 0:
+                            label += " B_" + item["entity"]
+                        else:
+                            label += " I_" + item["entity"]
+                else:
+                    for x in range(len(split)):
+                        label += " O"
+                        total_wc += 1
 
-        if label != "":
-            label += " "
-        label += "\t" + intent + "\n"
+            label += " " + intent + "\n"
 
-        origin += " EOS\t"
-#        print(origin)
-#        print(label)
+            origin += " EOS\t"
 
-        output_file.write(origin)
-        output_file.write(label)
+            output_iob.write(origin)
+            output_iob.write(label)
+
+        data_file.close()
+        output_iob.close()
+
+    output_stats.write("intent=\"{}\"\t".format(intent))
+    output_stats.write("Total_words:{}\t".format(total_wc))
+    slot_num = 1
+    sum_wc = 0
+    for key in entity_wc:
+        output_stats.write("slot{}=\"{}\":{}({:.1%})\t".format(slot_num, key, entity_wc[key], float(entity_wc[key])/total_wc))
+        sum_wc += entity_wc[key]
+        slot_num += 1
+    others_wc = total_wc-sum_wc
+    output_stats.write("others={}({:.1%})\t".format(others_wc, float(others_wc)/total_wc))
+    output_stats.write("\n")
+    output_stats.close()
